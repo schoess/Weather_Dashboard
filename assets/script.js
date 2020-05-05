cfd751d8b59fa9daa9980fc22f7c214e0c32adcd
nick <schoessnick@gmail.com>
2020-05-05 16:03:47 -0500

    building api connectivity

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
 function buildQueryURL() {
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}"
 
-    var key = "&appid=6c743e42a0f9ac97fab6ec81e5e3acc9"
-}
+    var baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    var key = "&appid=6c743e42a0f9ac97fab6ec81e5e3acc9";
+    var querySearch = $("#searchBar")
+        .val()
+        .trim();
+
+    return baseURL + querySearch + key;
+    
+};
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+
+    var queryURL = buildQueryURL();
+
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(console.log());
+});
