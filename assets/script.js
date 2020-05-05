514cc41addd9bd4c930b1d4918dff32af002777e
nick <schoessnick@gmail.com>
2020-05-05 11:45:08 -0500

    Built basic HTML page and crude styles for reference

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+function buildQueryURL() {
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}"
+
+    var key = "&appid=6c743e42a0f9ac97fab6ec81e5e3acc9"
+}
