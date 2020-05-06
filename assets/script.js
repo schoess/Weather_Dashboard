ac9547f545219a6bd4f294bdaac19a304a1e2ef2
Nick Schoess <schoessnick@gmail.com>
2020-05-06 13:47:03 -0500

    successfully calling api and returning both current weather and OneCall API's

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 function buildQueryURL() {
 
     var baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
-    var key = "&appid=6c743e42a0f9ac97fab6ec81e5e3acc9";
+    var key = "&units=imperial&appid=6c743e42a0f9ac97fab6ec81e5e3acc9";
     var querySearch = $("#searchBar")
         .val()
         .trim();
     var all = baseURL + querySearch + key;
     return baseURL + querySearch + key;
 };
-
 
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
@@ -18,13 +17,31 @@ $("#searchBtn").on("click", function(event) {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        //calls current weather API
+        $("#cityName").text(response.name);
+        $("#currentTemp").text("Temp: " + response.main.temp);
+        $("#currentHumidity").text("Humidity: " + response.main.humidity);
+        $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed);
+        $("#uvIndex").text("UV Index: " + response.main.temp);
+        var coord = response.coord;
+        //takes coordinates of city from first call and plugs them into the one-call 
+        //API in order to get the 5 day forecast and the UV index
+        console.log(coord.lat);
+        console.log(coord.lon);
+
+        var ocBaseURL = "http://api.openweathermap.org/data/2.5/onecall?lat=";
+        var ocLat = coord.lat;
+        var ocLon = "&lon=" + coord.lon;
+        var ocKey = "&units=imperial&appid=6c743e42a0f9ac97fab6ec81e5e3acc9";
+        console.log(ocBaseURL + ocLat + ocLon + ocKey);
+        return ocBaseURL + ocLat + ocLon + ocKey;
+    //catches possible error and throws error code
     }).catch(err => {
         // handle error here
         throw new Error(err)
     });
 
-//example methods to do the same thing
+//example methods to call an API with vanilla JS and Axios
     // fetch(queryURL)
     //     .then(res => res.json())
     //     .then(resJson => {
@@ -36,3 +53,4 @@ $("#searchBtn").on("click", function(event) {
     //         console.log(res.data)
     //     })
 });
+
