60db3930cfc7ced23ecae2de7c6f73bf536738ac
nick <schoessnick@gmail.com>
2020-05-06 14:53:10 -0500

    added momentJS to handle time append to 5 day forecast. Also successfully have 5 day forecast and uv index appendign to the correct places in HTML

--- assets/script.js
+++ assets/script.js
@@ -12,17 +12,15 @@ function buildQueryURL() {
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     var queryURL = buildQueryURL();
-    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function currentWeather(response) {
         //calls current weather API
         $("#cityName").text(response.name);
         $("#currentTemp").text("Temp: " + response.main.temp);
         $("#currentHumidity").text("Humidity: " + response.main.humidity);
         $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed);
-        $("#uvIndex").text("UV Index: " + response.main.temp);
         var coord = response.coord;
         //takes coordinates of city from first call and plugs them into the one-call 
         //API in order to get the 5 day forecast and the UV index
@@ -34,12 +32,40 @@ $("#searchBtn").on("click", function(event) {
         var ocLon = "&lon=" + coord.lon;
         var ocKey = "&units=imperial&appid=6c743e42a0f9ac97fab6ec81e5e3acc9";
         console.log(ocBaseURL + ocLat + ocLon + ocKey);
-        return ocBaseURL + ocLat + ocLon + ocKey;
+        var oneCallURL = ocBaseURL + ocLat + ocLon + ocKey;
+
+        $.ajax({
+            url: oneCallURL,
+            method: "GET"
+        }).then(function oneCallWeather(response) {
+            console.log(response);
+            $("#uvIndex").text("UV Index: " + response.current.uvi);
+            //5 day date append
+            $("#d1Date").text(moment().add(1, 'days').calendar());
+            $("#d2Date").text(moment().add(2, 'days').calendar());
+            $("#d3Date").text(moment().add(3, 'days').calendar());
+            $("#d4Date").text(moment().add(4, 'days').calendar());
+            $("#d5Date").text(moment().add(5, 'days').calendar());
+            //all 5 temperature appends
+            $("#d1Temp").text("Temp: " + response.daily[0].temp.day);
+            $("#d2Temp").text("Temp: " + response.daily[1].temp.day);
+            $("#d3Temp").text("Temp: " + response.daily[2].temp.day);
+            $("#d4Temp").text("Temp: " + response.daily[3].temp.day);
+            $("#d5Temp").text("Temp: " + response.daily[4].temp.day);
+            //all 5 humidity appends
+            $("#d1Humidity").text("Humidity: " + response.daily[0].humidity);
+            $("#d2Humidity").text("Humidity: " + response.daily[1].humidity);
+            $("#d3Humidity").text("Humidity: " + response.daily[2].humidity);
+            $("#d4Humidity").text("Humidity: " + response.daily[3].humidity);
+            $("#d5Humidity").text("Humidity: " + response.daily[4].humidity);
+
+        });
     //catches possible error and throws error code
     }).catch(err => {
         // handle error here
         throw new Error(err)
-    });
+    })});
+
 
 //example methods to call an API with vanilla JS and Axios
     // fetch(queryURL)
@@ -52,5 +78,5 @@ $("#searchBtn").on("click", function(event) {
     //     .then(res => {
     //         console.log(res.data)
     //     })
-});
+
 
